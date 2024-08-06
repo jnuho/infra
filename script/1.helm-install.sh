2cae150114c9c5f06d9c0623a2df7dbae5fbe256
jnuho <cactoos555@gmail.com>
2024-08-06 14:21:27 +0900

    .

--- script/1.helm-install.sh
+++ script/1.helm-install.sh
@@ -3,13 +3,12 @@
 current_context=$(kubectl config current-context)
 
 if [ "$current_context" = "minikube" ]; then
-    helm upgrade tst-release . -f values.local.yaml
+    helm install cat-release ../cat-chart -f ../cat-chart/values.local.yaml
 elif [ "$current_context" = "pi" ]; then
-    helm upgrade tst-release . -f values.pi.yaml
+    helm install cat-release ../cat-chart -f ../cat-chart/values.pi.yaml
 elif [[ "$current_context" == *"aws:eks"* ]]; then
     aws eks update-kubeconfig --region ap-northeast-2 --name my-cluster --profile terraform
-    helm upgrade tst-release . -f values.prd.AWS.L4.ingress.controller.yaml
+    helm install cat-release ../cat-chart -f ../cat-chart/values.prd.AWS.L4.ingress.controller.yaml
 else
     echo "NO KUBERNETES CONTEXT FOUND!"
 fi
-
