2097b07fc2b8278281ce3e920a58a0d0e103f067
jnuho <cactoos555@gmail.com>
2024-08-07 14:40:49 +0900

    .

--- thermal.sh
+++ thermal.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 
-HOST_WIDTH=5
-IP_WIDTH=15
+HOST_WIDTH=10
+IP_WIDTH=20
 TEMP_WIDTH=10
+TITLE_WIDTH=40
 
-echo "------------CPU temperature------------"
+echo ""
+title="𓇲  CPU °C"
+printf "%${TITLE_WIDTH}s%s\n" "$title"
 for host in "m" "w1" "w2"; do
     ip=$(ssh "$host" "hostname -I" | awk '{print $1}')
     temp=$(ssh "$host" sensors | grep 'cpu_thermal-virtual-0' -A 2 | awk '/temp1:/ {print $2}')
@@ -15,7 +18,8 @@ for host in "m" "w1" "w2"; do
 done
 
 echo ""
-echo "------------NVME SSD temperature------------"
+title="📥 SSD °C"
+printf "%${TITLE_WIDTH}s%s\n" "$title"
 for host in "m" "w1" "w2"; do
     ip=$(ssh "$host" "hostname -I" | awk '{print $1}')
     temp=$(ssh "$host" sensors | grep 'nvme-pci-0100' -A 2 | awk '/Composite:/ {print $2}')
@@ -25,7 +29,8 @@ for host in "m" "w1" "w2"; do
 done
 
 echo ""
-echo "------------Fan RPM------------"
+title="🍃 Fan RPM"
+printf "%${TITLE_WIDTH}s%s\n" "$title"
 for host in "m" "w1" "w2"; do
     ip=$(ssh "$host" "hostname -I" | awk '{print $1}')
     rpm=$(ssh "$host" sensors | grep 'pwmfan-isa-0000' -A 2 | awk '/fan1:/ {print $2 " " $3}')
@@ -34,3 +39,4 @@ for host in "m" "w1" "w2"; do
     printf "%-${HOST_WIDTH}s %-${IP_WIDTH}s %-${TEMP_WIDTH}s\n" "$host" "$ip" "$rpm"
 done
 
+echo ""
